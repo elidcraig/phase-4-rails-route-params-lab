68dc70704040b709b32edc277a7f8f1cb92216c2
Eli Craig <elidcraig@gmail.com>
2022-06-27 15:57:59 -0700

    commit tree 5d0b69c11d21f5e6d029574ffa4634b9fc3a2000

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    unless params[:name]
+      students = Student.all
+      render json: students
+    else
+      params_name = params[:name].capitalize
+      student = Student.where ["last_name = ? or first_name = ?", params_name, params_name]
+      # student = Student.where(first_name: params[:name]).or(Student.where(last_name: params[:name]))
+      render json: student
+    end    
+  end
+
+  def show
+    student = Student.find params[:id]
+    render json: student
   end
 
 end
